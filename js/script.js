a9c4f3d21bb724b3afa9ef1f439dd378ba449289
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-05-08 02:11:58 -0600

    added scroll animations on testimonial section

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ toggleBtn.onclick = function () {
     ? "fa-solid fa-xmark"
     : "fa-solid fa-bars"
 }
+
+
+// from "Subtle, yet Beautiful Scroll Animations" https://www.youtube.com/watch?v=T33NN_pPeNI 
+const observer = new IntersectionObserver((entries)=> {
+  entries.forEach((entry)=> {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('testimonial-show')
+    } else {
+      entry.target.classList.remove('testimonial-show');
+    }
+  });
+})
+
+
+const hiddenElements = document.querySelectorAll('.testimonial-hidden');
+hiddenElements.forEach((el) => observer.observe(el));
